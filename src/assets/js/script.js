159705cadc529b418bd36ef8cc0c8fe6787609ef
Lucas Gabriel da Silva Pires <lucas.pires@contagem.mg.gov.br>
2023-11-17 12:20:55 -0300

    Js com fonte

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -12,14 +12,20 @@ function Pokedex (){
     .then(res => res.json())
     .then(pokemon =>{
         var pokemonName = pokemon.name;
+        var pokemonTipo = pokemon.types.map(type => type.type.name).join(', '); 
         var pokemonAltura = pokemon.height/10;
         var pokemonPeso = pokemon.weight/10;
+        var pokemonHabilidade = pokemon.abilities.map(abilities => abilities.name).join(', ')
+        var pokemonRegiao = pokemon.main_region;
         var pokemonSprite = pokemon.sprites.front_default;
 
 
         result.innerHTML=`<h4 class="text-white text-center">Pokemon: ${pokemonName}</h4>
+        <h4 class="text-white text-center">Tipo: ${pokemonTipo} </h4>
         <h4 class="text-white text-center">Peso: ${pokemonAltura} Metros</h4>
         <h4 class="text-white text-center">Altura: ${pokemonPeso} Kilos</h4>
+        <h4 class="text-white text-center">Habilidades ${pokemonHabilidade} </h4>
+        <h4 class="text-white text-center">Região principal ${pokemonRegiao} </h4>
         <img src="${pokemonSprite}" class="img-fluid w-50 mx-auto d-block" alt="Imagem do Pokémon">
         `
         
