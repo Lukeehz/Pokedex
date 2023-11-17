b22116932472c26e6be8e8c22f1c0febceca5499
Lucas Gabriel <lucas.gab1010@gmail.com>
2023-11-17 18:11:00 -0300

    Terminado

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,12 +1,10 @@
 const pdex= document.querySelector('#pokedex') , result = document.querySelector('#result')
 const linkApi = 'https://pokeapi.co/api/v2/pokemon/'
 let idPokemon;
-
 function gerarNumeroAleatorio() {
-    idPokemon = Math.floor(Math.random() * 100) + 1;
+    idPokemon = Math.floor(Math.random() * 1010) + 1;
     return Promise.resolve(idPokemon);
 }
-
 function Pokedex (){
     fetch(`${linkApi}${idPokemon}`)
     .then(res => res.json())
@@ -15,20 +13,17 @@ function Pokedex (){
         var pokemonTipo = pokemon.types.map(type => type.type.name).join(', '); 
         var pokemonAltura = pokemon.height/10;
         var pokemonPeso = pokemon.weight/10;
-        var pokemonHabilidade = pokemon.abilities.map(abilities => abilities.name).join(', ')
-        var pokemonRegiao = pokemon.main_region;
+        var pokemonHabilidade = pokemon.abilities.map(ability => ability.ability.name).join(', ')
+        var pokemonRegiao = pokemon.pokemon_species;
         var pokemonSprite = pokemon.sprites.front_default;
-
-
-        result.innerHTML=`<h4 class="text-white text-center">Pokemon: ${pokemonName}</h4>
-        <h4 class="text-white text-center">Tipo: ${pokemonTipo} </h4>
-        <h4 class="text-white text-center">Peso: ${pokemonAltura} Metros</h4>
-        <h4 class="text-white text-center">Altura: ${pokemonPeso} Kilos</h4>
-        <h4 class="text-white text-center">Habilidades ${pokemonHabilidade} </h4>
-        <h4 class="text-white text-center">Região principal ${pokemonRegiao} </h4>
-        <img src="${pokemonSprite}" class="img-fluid w-50 mx-auto d-block" alt="Imagem do Pokémon">
-        `
-        
+        result.innerHTML=`
+        <h6 class="text-white text-center">Pokemon: ${pokemonName}</h6>
+        <h6 class="text-white text-center">Tipo: ${pokemonTipo} </h6>
+        <h6 class="text-white text-center">Peso: ${pokemonAltura} Kilos </h6>
+        <h6 class="text-white text-center">Altura: ${pokemonPeso} Metros </h6>
+        <h6 class="text-white text-center">Habilidades ${pokemonHabilidade} </h6>
+        <img src="${pokemonSprite}" class="img-fluid w-25 mx-auto d-block" alt="Imagem do Pokémon">
+        `  
     })
 }
 pdex.addEventListener('click',function(){
